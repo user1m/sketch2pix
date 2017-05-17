61751cc06a6c42ad553c569eac98f31ea2141939
DLVM <user1m@clmb-deeplearning.x4tbnmrspjuehheve522sy5m5c.jx.internal.cloudapp.net>
2017-05-17 04:46:57 +0000

    tested model and got results

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path/pix2pix
 
 time
-DATA_ROOT=../dataset/celebfaces/face2edge name=edge2face_generation which_direction=BtoA phase=val results_dir=results th test.lua
+env DATA_ROOT=../dataset/celebfaces/face2edge phase=test name=edge2face_generation which_direction=BtoA results_dir=results th test.lua
