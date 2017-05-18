a2465c29defbfe3d515283cc34fcfcd860d209ae
Claudius Mbemba <mbembac@gmail.com>
2017-05-18 09:32:02 -0700

    updated timing for test train scripts

--- test.sh
+++ test.sh
@@ -21,6 +21,5 @@ fi
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path/pix2pix
 
-time
-env DATA_ROOT=$2 phase=$4 name=$6 which_direction=$8 th test.lua
+time (env DATA_ROOT=$2 phase=$4 name=$6 which_direction=$8 th test.lua)
 #env DATA_ROOT=../dataset/celebfaces/face2edge phase=test name=edge2face_generation which_direction=BtoA results_dir=results th test.lua
