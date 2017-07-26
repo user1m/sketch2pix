fe2fe59714009bb13c6c17f0e6f9d24d61a098ef
Claudius Mbemba <mbembac@gmail.com>
2017-07-26 09:12:15 -0700

    updated test to take custom image input

--- pix2pix-pytorch/test.py
+++ pix2pix-pytorch/test.py
@@ -18,8 +18,11 @@ dataset = data_loader.load_data()
 model = create_model(opt)
 visualizer = Visualizer(opt)
 # create website
-web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
-webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+image_dir = opt.name if (opt.custom_image_dir == '') else opt.custom_image_dir
+web_dir = os.path.join(opt.results_dir, image_dir, '%s_%s' %
+                       (opt.phase, opt.which_epoch))
+webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (
+    opt.name, opt.phase, opt.which_epoch))
 # test
 for i, data in enumerate(dataset):
     if i >= opt.how_many:
